1e2637fb2dbfffc0400359be5deb7e6cc223c5ab
AnaBeatrizMAssuh <aaamassuh@gmail.com>
2022-01-13 15:44:51 -0300

    Projeto criado

---
+++ backend01/src/main/java/com/devsuperior/dsmovie01/Dsmovie01Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovie01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsmovie01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsmovie01Application.class, args);
+	}
+
+}
---
+++ backend01/src/test/java/com/devsuperior/dsmovie01/Dsmovie01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovie01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsmovie01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
